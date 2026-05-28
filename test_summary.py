97c5e07154de3f4522e04afa5a91acce21c16ebf
Filip Hultgren <hultgren.filip@gmail.com>
2026-05-28 20:05:25 +0200

    u

--- test_summary.py
+++ test_summary.py
@@ -3,7 +3,7 @@ from pyalpha import AsyncAlphaClient
 from pyalpha.definition.register_names import system_soc_n, system_active_power_n
 
 async def main():
-    client = AsyncAlphaClient(host='192.168.1.121', port=502)
+    client = AsyncAlphaClient(host='192.168.1.121', port=502, unit_id=1)
     await client.connect()
 
     soc = await client.get(system_soc_n)
