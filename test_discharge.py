b2de21e72550902d6b03f178b5253842423724d3
Filip Hultgren <hultgren.filip@gmail.com>
2026-08-13 16:23:39 +0200

    u

--- test_discharge.py
+++ test_discharge.py
@@ -40,6 +40,7 @@ async def main():
         total_load_power_n,
         bams_total_voltage_n,
         bams_total_current_n,
+        bams_actual_power_n,
         bams_undervoltage_charge_flag_n,
         bams_soc_calibration_flag_n,
     ]
